55b4fc2ea7fc4a5ab772265f4511005940e59acd
qxf <xfseven7@foxmail.com>
2021-01-11 02:15:36 +0700

    新增几个简单的问题

--- src/com/learn/chapter1/Lesson1.java
+++ src/com/learn/chapter1/Lesson1.java
@@ -11,14 +11,45 @@ public class Lesson1 {
 //        System.out.println(gcd1);
 
         // 二分查找
-        int[] data = {1, 2, 3, 4, 5, 7, 8, 11, 12, 23, 45, 55, 56, 67, 88, 89, 90, 123, 124, 125};
-        int i = searchBinary(126, data);
-        if (i == -1) {
-            System.out.println(-1);
-        } else {
-            System.out.println(data[i]);
-        }
+//        int[] data = {1, 2, 3, 4, 5, 7, 8, 11, 12, 23, 45, 55, 56, 67, 88, 89, 90, 123, 124, 125};
+//        int i = searchBinary(126, data);
+//        if (i == -1) {
+//            System.out.println(-1);
+//        } else {
+//            System.out.println(data[i]);
+//        }
 
+        // 1.1.1 给出以下表达式的值
+        int i = (0 + 15) / 2;// 盲猜7
+        System.out.println(i);
+        double v = 2.0e-6 * 100000000.1;// 盲猜200.2，猜错了
+        System.out.println(v);
+        boolean b = true && false || true && true;// 盲猜true
+        System.out.println(b);
+
+//        //1.1.2 给出以下表达式的类型和值
+////        (1 + 2.236) / 2; 盲猜double,值为1.618
+//        double v1 = (1 + 2.236) / 2;
+//        System.out.println(v1);
+////        1 + 2 + 3 + 4.0 盲猜double，值为10,值其实为10.0
+//        double v2 = 1 + 2 + 3 + 4.0;
+//        System.out.println(v2);
+////        4.1>=4 盲猜boolean，true
+//        boolean b1 = 4.1 >= 4;
+//        System.out.println(b1);
+////        1 + 2 + "3"  盲猜string，33
+//        String s = 1 + 2 + "3";
+//        System.out.println(s);
+
+//        1.1.3 编写一个程序，获取三个整数，如果都相等，打印equal，不相等打印not equal
+//        equal3(1, 1, 1);
+
+        
+
+    }
+
+    public static void equal3(int a, int b, int c) {
+        System.out.println(a == b && a == c ? "equal" : "not equal");
     }
 
     // region [最大公约数]
